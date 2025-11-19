a566c8c3b8e129165c9cb05f280b45fd4ef3f14c
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-11-19 15:25:51 -0300

    fix: cards

--- src/components/cards/CardFeatures.jsx
+++ src/components/cards/CardFeatures.jsx
@@ -19,7 +19,8 @@ function CardFeatures({
   };
 
   return (
-    <MotionDivDownToUp className="p-8 bg-white shadow-lg rounded-2xl hover:shadow-xl md:w-[20%] flex flex-col justify-center items-center">
+    <MotionDivDownToUp className="p-8 bg-white shadow-lg hover:shadow-xl md:w-[30%] flex flex-col justify-center items-center">
+      {" "}
       <div className="inline-flex items-center justify-center w-12 h-12 mb-6 bg-transparent border-2 border-solid rounded-lg text-darker/50 border-lighter">
         {Icone && <Icone size={24} strokeWidth={1.5} />}
       </div>
